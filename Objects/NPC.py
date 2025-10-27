4821adcce76ec9a999af029d6456ef3ee465a60b
Prototype0S <edwardcarroll2010@icloud.com>
2025-10-27 21:00:54 +1000

    Getting ready to add text banners

--- Objects/NPC.py
+++ Objects/NPC.py
@@ -1,13 +1,17 @@
 from GameFrame import RoomObject
-
+#from Text_banner_creation import Text_banner_creation  # Import your banner class
 
 class NPC(RoomObject):
-    """
-    The object for displaying the title
-    """
     def __init__(self, room, x, y):
         RoomObject.__init__(self, room, x, y)
-        
-        # set image
+
         image = self.load_image("Creep.png")
-        self.set_image(image,250,250)
+        self.set_image(image, 250, 250)
+
+    #    self.register_collision_object("Player")
+
+    #def handle_collision(self, other, other_type):
+    #    if other_type == "Player":
+    #        # Create banner object and add it to the room
+    #        banner = Text_banner_creation(self.room, 0, self.room.screen.get_height() - 300)
+    #        self.room.add_room_object(banner)
---
+++ Objects/Text_banner_creation.py
@@ -0,0 +1,8 @@
+#from GameFrame import RoomObject
+
+#class Text_banner_creation(RoomObject):
+#    def __init__(self, room, x, y):
+#        RoomObject.__init__(self, room, x, y)
+
+#        image = self.load_image("Text_Banner.png")
+#        self.set_image(image, 1920, 300)
